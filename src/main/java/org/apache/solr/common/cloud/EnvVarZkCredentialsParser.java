a673730d5fb39e2ceacccb1016108ddd98e95e12
notenoteme <git@notenote.me>
2022-08-16 00:00:20 +0200

    fix: updated commons-lang dependency

--- src/main/java/org/apache/solr/common/cloud/EnvVarZkCredentialsParser.java
+++ src/main/java/org/apache/solr/common/cloud/EnvVarZkCredentialsParser.java
@@ -1,7 +1,7 @@
 package org.apache.solr.common.cloud;
 
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.zookeeper.ZooDefs;
 
 import java.util.Arrays;
